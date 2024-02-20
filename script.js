50669eefb871f2521c267c5c00aa3154f7a89b45
Thomas Fraser <150303640+TheRealThomasFraser@users.noreply.github.com>
2024-02-20 07:03:05 +0000

    trial 1

---
+++ script.js
@@ -0,0 +1,8 @@
+function abtDrpDwn() {
+    var section = document.getElementById("dropdown-content");
+    if (section.style.display == "none") {
+    section.style.display = "block";
+    } else {
+      section.style.display = "none"
+    }
+  }
