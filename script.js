67a7689be8fa6f8c98aa95bb638099b511aa63ce
Thomas Fraser <150303640+TheRealThomasFraser@users.noreply.github.com>
2024-02-22 04:07:17 +0000

    settings popup trial numero uno

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function abtDrpDwn() {
   var section = document.getElementById("dropdown-content");
   if (section.style.display == "none") {
-    section.style.display = "block";
+  section.style.display = "block";
   } else {
     section.style.display = "none"
   }
+}
+
+function showSettings() {
+  var section = document.getElementById("settingsPopup");
+  if (section.style.display == "none") {
+  section.style.display = "block";
+  } else {
+    section.style.display = "none"
+  }
+}
+
+function enlargeText() {
+  var elements =
+document. querySelectorAll('body *');
+elements. forEach(function (element) {
+var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+var newSize =
+(parseFloat (currentSize) * 1.2) + 'px';
+element.style.fontSize = newSize;
+}) ;
 }
