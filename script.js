77b7de92f0befab335a361820c06dff1390c9cc0
Thomas Fraser <150303640+TheRealThomasFraser@users.noreply.github.com>
2024-02-25 04:25:11 +0000

    settings popup test numero dos

--- script.js
+++ script.js
@@ -22,7 +22,26 @@ document. querySelectorAll('body *');
 elements. forEach(function (element) {
 var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
 var newSize =
-(parseFloat (currentSize) * 1.2) + 'px';
+(parseFloat (currentSize) * 1.25) + 'px';
 element.style.fontSize = newSize;
+}) ;
+}
+
+function shrinkText() {
+  var elements =
+document. querySelectorAll('body *');
+elements. forEach(function (element) {
+var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+var newSize =
+(parseFloat (currentSize) * 0.75) + 'px';
+element.style.fontSize = newSize;
+}) ;
+}
+
+function changeFont() {
+  var elements =
+document. querySelectorAll('body *');
+elements. forEach(function (element) {
+element.style.fontFamily = 'Helvetica';
 }) ;
 }
